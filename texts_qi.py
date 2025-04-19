3174def8b3de8f813ff68abf97c9eb4d54de58d7
flavioolimpio <flavio_olimpio@outlook.com>
2025-04-19 17:11:09 -0300

    Update texts_qi.py
    add links khan academy q1

--- texts_qi.py
+++ texts_qi.py
@@ -19,12 +19,17 @@ class TextsQI:
             <li>Exercícios de aplicação para fixação dos tópicos: classificação, separação e modelos atômicos. </li>
         </ol>
 
-        <h1 style='text-align: center; color: black;'>Atividades Complementares</h1>
-        <p style='text-align: justify; color: black;'>Explore recursos online para aprofundar seu aprendizado:</p>
-        <ul style='text-align: justify; color: black;'>
-            <li><a href="https://phet.colorado.edu/sims/html/build-an-atom/latest/build-an-atom_en.html" target="_blank">PhET Build an Atom</a> – Monte diferentes átomos interativamente.</li>
-            <li><a href="https://phet.colorado.edu/sims/html/rutherford-scattering/latest/rutherford-scattering_en.html" target="_blank">PhET Rutherford Scattering</a> – Simule o experimento de Rutherford e observe desvios de partículas.</li>
-            <li><a href="https://www.youtube.com/watch?v=nDyYtgeD3D0" target="_blank">Vídeo: Experimento de Thomson</a> – Demonstração do tubo de raios catódicos.</li>
-            <li><a href="https://www.youtube.com/watch?v=AHXD9pgmkPQ" target="_blank">Vídeo: Experimento de Rutherford</a> – Detalhes da descoberta do núcleo atômico.</li>
-        </ul>
-        '''
+        Aqui está o trecho HTML atualizado com os três novos links da Khan Academy incluídos no final da lista de atividades complementares:
+
+    <h1 style='text-align: center; color: black;'>Atividades Complementares</h1>
+    <p style='text-align: justify; color: black;'>Explore recursos online para aprofundar seu aprendizado:</p>
+    <ul style='text-align: justify; color: black;'>
+        <li><a href="https://phet.colorado.edu/sims/html/build-an-atom/latest/build-an-atom_en.html" target="_blank">PhET Build an Atom</a> – Monte diferentes átomos interativamente.</li>
+        <li><a href="https://phet.colorado.edu/sims/html/rutherford-scattering/latest/rutherford-scattering_en.html" target="_blank">PhET Rutherford Scattering</a> – Simule o experimento de Rutherford e observe desvios de partículas.</li>
+        <li><a href="https://www.youtube.com/watch?v=nDyYtgeD3D0" target="_blank">Vídeo: Experimento de Thomson</a> – Demonstração do tubo de raios catódicos.</li>
+        <li><a href="https://www.youtube.com/watch?v=AHXD9pgmkPQ" target="_blank">Vídeo: Experimento de Rutherford</a> – Detalhes da descoberta do núcleo atômico.</li>
+        <li><a href="https://pt.khanacademy.org/science/quimica-ensino-medio/x57f96b935ba57f4b:introducao-a-quimica/x57f96b935ba57f4b:tudo-e-feito-de-atomos/a/matter-elements-atoms-article" target="_blank"> Introdução aos elementos e átomos.</li>
+        <li><a href="https://pt.khanacademy.org/science/quimica-ensino-medio/x57f96b935ba57f4b:modelos-atomicos/x57f96b935ba57f4b:introducao-modelos-atomicos/a/daltons-atomic-theory-version-2" target="_blank">Entenda os postulados de Dalton.</li>
+        <li><a href="https://pt.khanacademy.org/science/quimica-ensino-medio/x57f96b935ba57f4b:modelos-atomicos/x57f96b935ba57f4b:introducao-modelos-atomicos/a/discovery-of-the-electron-and-nucleus" target="_blank">Descoberta do elétron e do núcleo.</li>
+    </ul>
+    '''
