a3982cc5f9f8450c185266d254993765f99e04d9
flavioolimpio <34543135+flavioolimpio@users.noreply.github.com>
2025-07-08 10:57:18 -0300

    Update constants.py
    update

--- constants.py
+++ constants.py
@@ -9,8 +9,8 @@ AULAS_QI = [
 AULAS_QII = [
     "Escolha uma Aula",
     "1° Bimestre: Estequiometria e Estudos dos Gases",
-    "2° Bimestre: Soluções e Propriedades Coligativas",
-    "3° Bimestre: Termoquímica e Eletroquímica",
+    "2° Bimestre: Termoquímica e Eletroquímica",
+    "3° Bimestre: Soluções e Propriedades Coligativas",
     "4° Bimestre:  Cinética e Equilibrio Químico",
 ]
 
